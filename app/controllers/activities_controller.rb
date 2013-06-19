544239f749e610de9935ad22b5f9fb93d5c06d97
Eric Liu <dathtohn@gmail.com>
2013-06-19 15:27:24 -0400

    only see friends activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,7 @@
 class ActivitiesController < ApplicationController
   def index
   	if signed_in?
-	  	@activities = PublicActivity::Activity.order("created_at desc")#.where(owner_id: current_user.friend_ids, owner_type: "User")
+	  	@activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friend_ids, owner_type: "User")
 	  else
 	  	redirect_to home_path
 	  end
