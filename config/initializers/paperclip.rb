79e6059ff1e9eb281fd2c449a5fe4798e5283930
Eric Liu <dathtohn@gmail.com>
2013-06-25 11:08:11 -0400

    s3

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,6 @@
+# config/initializers/paperclip.rb
+# require "paperclip"
+
+# Paperclip.options[:command_path] = "/ImageMagick"
+# Paperclip::Attachment.default_options[:url] = 'my-art.s3.amazonaws.com/'
+# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
